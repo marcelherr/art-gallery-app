42dd3c1597257f982b8f1a626c2338635dd9f380
Marcel Herr <mrclhrr@gmail.com>
2024-04-12 15:40:26 +0200

    implement solution feature list of all art pieces

---
+++ components/ArtPiecePreview.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function ArtPiecePreview({ image, title, artist }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <Image src={image} alt="art-piece" width={240} height={130} />
+      <h3>{artist}</h3>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import ArtPieces from "@/components/ArtPieces";
+import useSWR from "swr";
+
+const URL = "https://example-apis.vercel.app/api/art";
+
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(URL);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <ArtPieces pieces={data} />
     </div>
   );
 }
