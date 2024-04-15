aaa680c1b723a7f9ad8fe0c839fda79678a419fc
Marcel Herr <mrclhrr@gmail.com>
2024-04-15 12:06:40 +0200

    implement the Art Piece Details component'

--- components/ArtPieceDetails.js
+++ components/ArtPieceDetails.js
@@ -3,10 +3,10 @@ import Image from "next/image";
 export default function ArtPieceDetails({ image, title, artist, year, genre }) {
   return (
     <article>
-      <h2>{title}</h2>
+      <h1>{title}</h1>
       <small>{year}</small>
       <Image src={image} alt="art-piece" width={240} height={130} />
-      <h3>{artist}</h3>
+      <h2>{artist}</h2>
       <p> {genre}</p>
     </article>
   );
