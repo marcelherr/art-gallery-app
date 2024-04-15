97c88cd15a6010a65a14bfcbf03a58af2e62d2a2
Marcel Herr <mrclhrr@gmail.com>
2024-04-15 12:07:39 +0200

    implement the slug.js

--- pages/ArtPieces/[slug].js
+++ pages/ArtPieces/[slug].js
@@ -1,5 +1,44 @@
 import ArtPieceDetails from "@/components/ArtPieceDetails";
+import { useRouter } from "next/router";
+import styled from "styled-components";
 
-export default function Slug() {
-  return <ArtPieceDetails />;
+const StyledButton = styled.button`
+  background-color: #0a0a23;
+  color: #fff;
+  border: none;
+  border-radius: 10px;
+  padding: 15px;
+  min-height: 30px;
+  min-width: 120px;
+`;
+
+export default function ArtPieces({ data }) {
+  const router = useRouter();
+  const { slug } = router.query;
+  const currentArtPiece = data.find((artPiece) => artPiece.slug === slug);
+
+  if (!currentArtPiece) {
+    return null;
+  }
+
+  return (
+    <div>
+      <ArtPieceDetails
+        image={data.image}
+        title={data.title}
+        artist={data.artist}
+        year={data.year}
+        genre={data.genre}
+      />
+      <StyledButton
+        type="button"
+        name="back-button"
+        onClick={() => {
+          router.push("../index");
+        }}
+      >
+        Back
+      </StyledButton>
+    </div>
+  );
 }
